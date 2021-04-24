c9ae3bb3f6d540dc6c2c1ed537776ffc93732b27
amacabr2 <anthony.macabrey70@gmail.com>
2021-04-25 00:02:06 +0200

    todo entity

---
+++ lib/entities/Todo.dart
@@ -0,0 +1,46 @@
+enum TodoStatus {
+  PENDING,
+  CANCELED,
+  DONE,
+}
+
+class Todo {
+  String id;
+
+  String title;
+
+  String description;
+
+  TodoStatus status;
+
+  DateTime createdAt;
+
+  DateTime mustDoneAt;
+
+  Todo({
+    this.id,
+    this.title,
+    this.description,
+    this.status,
+    this.createdAt,
+    this.mustDoneAt
+  });
+
+  factory Todo.fromJson(Map<String, dynamic> json) => Todo(
+    id: json['id'] as String,
+    title: json['title'] as String,
+    description: json['description'] as String,
+    status: json['status'] as TodoStatus,
+    createdAt: json['createdAt'] as DateTime,
+    mustDoneAt: json['mustDoneAt'] as DateTime,
+  );
+  
+  Map<String, dynamic> toJson() => <String, dynamic>{
+    'id': this.id,
+    'points': this.title,
+    'description': this.description,
+    'status': this.status,
+    'createdAt': this.createdAt,
+    'mustDoneAt': this.mustDoneAt,
+  };
+}
